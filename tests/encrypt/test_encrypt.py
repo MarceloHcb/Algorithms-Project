408685c9621d12c5ce8774658db66821f30efc42
MarceloHcb <marcelohcb@msn.com>
2023-07-08 20:09:15 -0300

    tests

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('Python', 1) == 'P_nohty'
+    assert encrypt_message('Python', 2) == 'noht_yP'
+    assert encrypt_message('Python', 3) == 'tyP_noh'
+    assert encrypt_message('Python', 4) == 'no_htyP'
+    assert encrypt_message('Python', 5) == 'ohtyP_n'
+    assert encrypt_message('Python', 6) == 'nohtyP'
+    assert encrypt_message('Python', 9) == 'nohtyP'
+    with pytest.raises(TypeError):
+        encrypt_message(3, '1')
