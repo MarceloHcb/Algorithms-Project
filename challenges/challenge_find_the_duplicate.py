12d2734444ae61c286297e5d9c6f30c635c33e70
MarceloHcb <marcelohcb@msn.com>
2023-07-08 18:57:52 -0300

    nomeros repetidos, req5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or isinstance(nums, str):
+        return False
+    list_num = []
+    for n in nums:
+        if n in list_num:
+            return n
+        elif not isinstance(n, int) or n < 1 or n > len(nums) - 1:
+            return False
+        list_num.append(n)
+    return False
