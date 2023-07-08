da807116d71fc3c8ec4f8847c8061425aee92173
MarceloHcb <marcelohcb@msn.com>
2023-07-08 17:06:43 -0300

    algoritimo de busca

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+    students = 0
+    for period in permanence_period:
+        if not all(isinstance(p, int) for p in period):
+            return None
+        if period[0] <= target_time <= period[1]:
+            students += 1
+            return students
